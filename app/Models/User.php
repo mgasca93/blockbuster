5e23a3d9a0ba2dab7b4df91f72605c6652d29dcf
Mario Gasca <web@mariogasca.com>
2023-11-22 20:31:43 -0600

    Se agrego el scope global para obtener solo usuarios no eliminados

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Scopes\UserScope;
 
 class User extends Authenticatable
 {
@@ -45,4 +46,13 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
+    protected static function booted(): void
+    {
+        static::addGlobalScope( new UserScope );
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }

b32dc1c23831d28ce041ac03b12b0eb289baf5ad
Mario Gasca <web@mariogasca.com>
2023-11-25 16:40:42 -0600

    Se crea el rol General User y se asigna a los usuarios del seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -14,40 +16,24 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+        User::truncate();
+        Role::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+
+        $user = Role::create([
+            'name'          => 'General User',
+            'guard_name'    => 'web'
+        ]);
+
         User::create([
             'firstname'     => 'Mario Aurelio',
             'lastname'      => 'Gasca López',
             'slug'          => 'mario-aurelio-gasca-lopez',
             'email'         => 'hola@mariogasca.com',
             'password'      => Hash::make('password')
-        ]);
-        User::create([
-            'firstname'     => 'Ricardo Daniel',
-            'lastname'      => 'Gasca López',
-            'slug'          => 'ricardo-daniel-gasca-lopez',
-            'email'         => 'hola@ricardogasca.com',
-            'password'      => Hash::make('password')
-        ]);
-        User::create([
-            'firstname'     => 'Aurelio',
-            'lastname'      => 'Gasca Carrillo',
-            'slug'          => 'aurelio-gasca-carrillo',
-            'email'         => 'aurelio.gasca.carrillo@gmail.com',
-            'password'      => Hash::make('password')
-        ]);
-        User::create([
-            'firstname'     => 'Reyna Esmeralda',
-            'lastname'      => 'Dominguez Trujillo',
-            'slug'          => 'reyna-esmeralda-dominguez-trujillo',
-            'email'         => 'rdominguez@gmail.com',
-            'password'      => Hash::make('password')
-        ]);
-        User::create([
-            'firstname'     => 'Jospeh Nikolay',
-            'lastname'      => 'Gasca Dominguez',
-            'slug'          => 'joseph-nikolay-gasca-dominguez',
-            'email'         => 'joseph.nikolay.gasca@gmail.com',
-            'password'      => Hash::make('password')
-        ]);
+        ])->assignRole( $user );   
+
     }
 }

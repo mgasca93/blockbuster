d0ee9faa27508a5431a3b3d966cc3390461d5424
Mario Gasca <web@mariogasca.com>
2023-11-26 18:58:13 -0600

    Se agrego la ruta para el logout

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\v1\UserController as UserControllerv1;
+use App\Http\Controllers\API\v1\LoginController as Loginv1;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,10 +17,15 @@ use App\Http\Controllers\API\v1\UserController as UserControllerv1;
 
 Route::prefix('v1')->group(function(){
     Route::middleware('auth:sanctum')->group(function(){
-        Route::resource('users', UserControllerv1::class )->names('api.users')->only(['index', 'show', 'store', 'update', 'destroy']);    
+        Route::resource('users', UserControllerv1::class )->names('api.users')->only(['index', 'show', 'store', 'update', 'destroy']);            
     });
-});
+    
+    Route::middleware('auth:sanctum')->get('/logout', function (Request $request) {
+        // Revoke all tokens and revoke the token that was used to authenticate the current request
+        $request->user()->tokens()->delete();
+        $request->user()->currentAccessToken()->delete();
+        return response()->json(['message' => 'User logout with success'], 200);
+    });
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+    Route::post('login',[Loginv1::class, 'store'])->name('api.login');
+});
